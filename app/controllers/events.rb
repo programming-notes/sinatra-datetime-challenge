886e85c7f1ee78c48ef041e73a224101f1b64d65
Torey Hickman <torey@toreyhickman.com>
2016-04-24 19:29:00 -0400

    Edit an event

--- app/controllers/events.rb
+++ app/controllers/events.rb
@@ -4,13 +4,13 @@ get "/events" do
   erb :"events/index"
 end
 
-get '/events/new' do
+get "/events/new" do
   @event = Event.new
 
   erb :"events/new"
 end
 
-post '/events' do
+post "/events" do
   puts params[:event]
   @event = Event.new(params[:event])
 
@@ -19,4 +19,27 @@ post '/events' do
   else
     erb :"events/new"
   end
+end
+
+get "/events/:id/edit" do
+  @event = Event.find(params[:id])
+
+  erb :"events/edit"
+end
+
+put "/events/:id" do
+  @event = Event.find(params[:id])
+  @event.assign_attributes(params[:event])
+
+  if @event.save
+    redirect "/events/#{@event.id}"
+  else
+    erb :"events/edit"
+  end
+end
+
+get "/events/:id" do
+  @event = Event.find(params[:id])
+
+  erb :"events/show"
 end
